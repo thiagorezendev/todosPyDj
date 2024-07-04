1303c4d9af3604870b065b51d9865d5c35ec7129
thiagorezendev <thiagor3zende@gmail.com>
2024-07-04 18:22:44 -0300

    funcionalidade de conclusão 80%

--- setup/urls.py
+++ setup/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from todos.views import TodoListView, TodoCreateView, TodoUpdateView, TodoDeleteView
+from todos.views import TodoListView, TodoCreateView, TodoUpdateView, TodoDeleteView, TodoCompleteView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +24,5 @@ urlpatterns = [
     path('create', TodoCreateView.as_view(), name="todo_create"),
     path('update/<int:pk>', TodoUpdateView.as_view(), name="todo_update"),
     path('delete/<int:pk>', TodoDeleteView.as_view(), name="todo_delete"),
+    path('complete/<int:pk>', TodoCompleteView.as_view(), name="todo_complete")
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,7 +1,9 @@
 # from django.shortcuts import render
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, View
 from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404, redirect
 from .models import Todo
+from datetime import date
 
 # def todo_list(request):
 #     todos = Todo.objects.all()
@@ -23,3 +25,10 @@ class TodoUpdateView(UpdateView):
 class TodoDeleteView(DeleteView):
     model = Todo
     success_url = reverse_lazy("todo_list")
+
+class TodoCompleteView(View):
+    def get(self, request, pk):
+        todo = get_object_or_404(Todo, pk=pk)
+        todo.finished_at = date.today()
+        todo.save()
+        return redirect("todo_list")
